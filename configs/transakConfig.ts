d7d09fc05aebefb0124e566a7284be574ba4a8c8
ashutosh887 <ashutoshj887@gmail.com>
2024-03-20 16:51:41 +0530

    feat: add dotenv support

--- configs/transakConfig.ts
+++ configs/transakConfig.ts
@@ -1,7 +1,9 @@
 import { Transak, TransakConfig } from "@transak/transak-sdk";
 
+const apiKey = process.env.EXPO_PUBLIC_TRANSAK_API_KEY;
+
 const transakConfig: TransakConfig = {
-  apiKey: "API_KEY",
+  apiKey,
   environment: Transak.ENVIRONMENTS.STAGING,
   exchangeScreenTitle: "React Native Expo Demo",
 };
